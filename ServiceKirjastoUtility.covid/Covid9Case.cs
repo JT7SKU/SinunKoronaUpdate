bb0b2887929b0b037159a346c31ad4cb76c7819d
Jani Turunen <janisku7@gmail.com>
2020-03-29 00:19:31 +0200

    Koronathings

---
+++ ServiceKirjastoUtility.covid/Covid9Case.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ServiceKirjastoUtility.covid
+{
+
+    public partial class Covid9Case
+    {
+        [JsonPropertyName("link")]
+        public Link Link { get; set; }
+
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+
+        [JsonPropertyName("class")]
+        public string Class { get; set; }
+
+        [JsonPropertyName("updated")]
+        public DateTimeOffset Updated { get; set; }
+    }
+
+    public partial class Link
+    {
+        [JsonPropertyName("item")]
+        public List<Item> Item { get; set; }
+    }
+
+    public partial class Item
+    {
+        [JsonPropertyName("href")]
+        public Uri Href { get; set; }
+
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+
+        [JsonPropertyName("class")]
+        public string Class { get; set; }
+    }
+}
